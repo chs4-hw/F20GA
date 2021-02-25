b406f1ddf5fc610a8537843849cd3fbf82df957a
chs4-hw <56627743+chs4-hw@users.noreply.github.com>
2021-02-25 10:47:36 +0000

    Update Melee.cs

--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Melee : MonoBehaviour
-    // Mellee attack varibles
+    // Melee attack varibles
     {
         public float damage;
         public float range;
